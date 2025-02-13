55eaf9e9da67b81928eebaa346b7a1c85d9e70e2
Anat <anat.inako@gmail.com>
2025-02-13 17:08:36 +0100

    update doc

--- src/__init__.py
+++ src/__init__.py
@@ -6,5 +6,5 @@ A Python client for interacting with the ClinicalTrials.gov API v2.
 
 from .client import ClinicalTrialsAPI, ClinicalTrialParser
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 __all__ = ["ClinicalTrialsAPI", "ClinicalTrialParser"]
