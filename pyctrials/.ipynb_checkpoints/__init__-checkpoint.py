9bed3be37eb4457cbf65b949e48e5b9c13388c59
Anat <anat.inako@gmail.com>
2025-02-13 19:40:23 +0100

    Fix package structure

--- pyctrials/.ipynb_checkpoints/__init__-checkpoint.py
+++ pyctrials/.ipynb_checkpoints/__init__-checkpoint.py
@@ -4,7 +4,7 @@ ClinicalTrials.gov API Client
 A Python client for interacting with the ClinicalTrials.gov API v2.
 """
 
-from .client import ClinicalTrialsAPI, ClinicalTrialParser
+from .pyctrials import ClinicalTrialsAPI, ClinicalTrialParser
 
 __version__ = "0.1.8"
 __all__ = ["ClinicalTrialsAPI", "ClinicalTrialParser"]
