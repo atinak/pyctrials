64ade6cc6ba8fbedc81664b745efcba4454cf19a
Anat <anat.inako@gmail.com>
2025-02-13 15:45:52 +0100

    Bump version to 0.1.1

---
+++ src/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,10 @@
+"""
+ClinicalTrials.gov API Client
+
+A Python client for interacting with the ClinicalTrials.gov API v2.
+"""
+
+from .client import ClinicalTrialsAPI, ClinicalTrialParser
+
+__version__ = "0.1.1"
+__all__ = ["ClinicalTrialsAPI", "ClinicalTrialParser"]
